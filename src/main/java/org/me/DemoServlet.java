589e74fa50855b896d8a6d05a71253e25f913211
Dave Moten <davidmoten@gmail.com>
2013-03-02 08:48:55 +1100

    remove servlet name from annotation

--- src/main/java/org/me/DemoServlet.java
+++ src/main/java/org/me/DemoServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="Demonstration",value="/sayHello")
+@WebServlet(value="/sayHello")
 public class DemoServlet extends HttpServlet {
 
 	@Override
