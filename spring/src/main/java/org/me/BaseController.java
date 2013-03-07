09d21324480d9392a40044a9d87b5e1814662408
Dave Moten <davidmoten@gmail.com>
2013-03-07 15:20:10 +1100

    refactor spring package

---
+++ spring/src/main/java/org/me/BaseController.java
@@ -0,0 +1,33 @@
+package org.me;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class BaseController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String welcome(ModelMap model) {
+
+		model.addAttribute("message",
+				"Maven Web Project + Spring 3 MVC - welcome()");
+
+		// Spring uses InternalResourceViewResolver and return back index.jsp
+		return "index";
+
+	}
+
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String welcomeName(@PathVariable String name, ModelMap model) {
+
+		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - "
+				+ name);
+		return "index";
+
+	}
+
+}
